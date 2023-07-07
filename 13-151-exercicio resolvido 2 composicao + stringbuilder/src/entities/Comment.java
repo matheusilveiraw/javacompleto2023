646245c641a6e48d938d3072ddab1af2217fbcd2
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-07-07 17:47:03 -0300

    exer de fixacao composicao com string builder

---
+++ 13-151-exercicio resolvido 2 composicao + stringbuilder/src/entities/Comment.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Comment {
+	
+	private String text;
+	
+	public Comment() { }
+
+	public Comment(String text) {
+		super();
+		this.text = text;
+	}
+
+	public String getText() {
+		// TODO Auto-generated method stub
+		return text;
+	}
+}
