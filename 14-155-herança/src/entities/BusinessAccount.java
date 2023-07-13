f2099a144679833ff9307965cef955701ead2fe9
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-07-13 16:53:33 -0300

    aula de herança

---
+++ 14-155-herança/src/entities/BusinessAccount.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class BusinessAccount extends Account { //extends define que tudo que tem em accoumt eui consigo usar aqui
+	
+	private double loanLimit;
+	
+	public BusinessAccount() {
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, double loanLimit) { //criado um construtor que usa os argumentos de account
+		super(number, holder, balance); //é pra isso que serve o super, pra usar os argumentos da superclasse (classe que é herdada)
+		this.loanLimit = loanLimit;
+	}
+	
+
+	public double getLoanLimit() {
+		return loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if(amount <= loanLimit) { 
+			balance += amount - 10.0;
+		}
+	}
+	
+	public void setLoanLimit(double loanLimit) {
+		this.loanLimit = loanLimit;
+	};
+	
+	
+	
+	
+}
