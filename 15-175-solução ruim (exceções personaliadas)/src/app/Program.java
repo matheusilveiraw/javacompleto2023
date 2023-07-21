835572d7d3671e907e2db6810dffb7f44956186d
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-07-21 01:08:54 -0300

    solução menos ruim (maior parte da solução na class reservation)

---
+++ 15-175-solução ruim (exceções personaliadas)/src/app/Program.java
@@ -0,0 +1,48 @@
+package app;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException { //parseexcepetion = permitindo que o metodo main pode lançar esse tipo de exceção, caso ela aconteça
+		//solução ruim - lógica de validação no programa principal
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Room number: ");
+		int number = sc.nextInt();
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: check-out date must be after check-in date");
+		} else { 
+			Reservation reservation = new Reservation(number, checkIn, checkOut); 
+			System.out.println("Reservation: " + reservation); //vai usar o to string
+			
+			System.out.println();
+			System.out.print("Enter date to update the reservation: ");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) { 
+				System.out.println("Error in reservation: " + error);
+			} else { 
+				System.out.println("Reservation: " + reservation);
+			}
+		}
+		sc.close();
+	}
+
+}
