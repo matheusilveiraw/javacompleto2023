ca9de1a73418427b75276c86e9db42ec186ca8f1
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-03 15:40:32 -0300

    exerc

--- 17-220- exerc proposto/src/app/Program.java
+++ 17-220- exerc proposto/src/app/Program.java
@@ -1,54 +1,70 @@
 package app;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Product;
+
 public class Program {
 
-	public static void main(String[] args) throws IOException {
-		//no arquivo tá produto, preço, quantidade,tem que fazer um novo arquivo com produto + preço total
-		Scanner sc = null;
+	public static void main(String[] args) throws ParseException {
+
 		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Product> list = new ArrayList<>();
+
+		System.out.println("Enter file path: ");
+		String sourceFileStr = sc.nextLine();
+
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
 		
-		File file = new File("c:\\temp\\exerproposot\\entrada.txt");	
-		FileWriter escritor = new FileWriter("c:\\\\temp\\\\exerproposot\\\\saida.txt");
+		boolean success = new File(sourceFolderStr + "\\out").mkdir();
 		
-		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				String linha = sc.nextLine();
-				String linhas[] = linha.split(",");
-				Integer.parseInt(linhas[1]);
-				String produto = linhas[0];
-				
-				System.out.println("Produto: " + produto);
-				String valorProv = linhas[1] + "." + linhas[2];
-				double valor = Double.parseDouble(valorProv); //da um erro se tentar fazer tudo em uma linha, deixar assim
-				int quantidade = Integer.parseInt(linhas[3]);
-				System.out.println("Total: " + (valor * quantidade));
-								
-				escritor.write("Produto: " + produto + "." + " Total: R$" + valor * quantidade);
-				escritor.close();
-							}
-		} catch(IOException e) { 
-			System.out.println("Error: " + e.getMessage());
-		} finally {
-			if(sc != null) { 
-				sc.close();	
+		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+			String itemCsv = br.readLine();
+			while (itemCsv != null) {
+
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+
+				list.add(new Product(name, price, quantity));
+
+				itemCsv = br.readLine();
 			}
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+				for (Product item : list) {
+					bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+
+				System.out.println(targetFileStr + " CREATED!");
+				
+			} catch (IOException e) {
+				System.out.println("Error writing file: " + e.getMessage());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
 		}
-		
-//		
-//		System.out.println("Digite os produtos: ");
-//		for(int i = 0; i < 4; i++) { 
-//			System.out.println("Produto #" + (i + 1) + ":");
-//			String produto = sc.nextLine();
-//			System.out.println(produto);
-//		} 
+
 		sc.close();
 	}
 }
---
+++ 17-220- exerc proposto/src/entities/Product.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class Product {
+	
+	private String name;
+	private Double price; 
+	private Integer quantity;
+	
+	public double total( ) {
+		return quantity * price;
+	}
+	
+	public Product(String name, Double price, Integer quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+}
