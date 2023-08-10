793e1c7bfe821db68f7a8c0b406a6f3dfa3d9ab9
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-10 16:08:09 -0300

    contratos

---
+++ 18-229-exerc de fixação interface/src/entities/ContractService.java
@@ -0,0 +1,17 @@
+package entities;
+
+import services.OnlinePaymentService;
+
+public class ContractService {
+	
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	public void processContract(Contract contract, int months) {
+		
+	}
+
+}
