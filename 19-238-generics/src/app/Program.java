9cf67f3751c9592f32fc46488304c28aa70f942f
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-19 19:30:19 -0300

    aula 238

--- 19-238-generics/src/app/Program.java
+++ 19-238-generics/src/app/Program.java
@@ -1,10 +1,32 @@
 package app;
 
+import java.util.Scanner;
+
+import services.PrintService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		//generics //teste internet
+		//generics 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService<String> ps = new PrintService<>();
+		//poderia estar chamando um integer aqui, só tem que mudar o scanner lá embaixo
+		
+		System.out.println("How many values");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) { 
+			String value = sc.next();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		
+		System.out.println("First: " + ps.first());
 
+		sc.close();
 	}
 
 }
