84635543e61d12dcc821da5f8c57e180b943dd4d
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-23 14:39:39 -0300

    aula 269 finalizada funfando

--- 21-268- primeiro projeto com banco de dados/src/db/DB.java
+++ 21-268- primeiro projeto com banco de dados/src/db/DB.java
@@ -8,43 +8,41 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
-	
+
 	private static Connection conn = null;
 	
-		
-	public static Connection getConnection()  {
-		
-		if(conn == null) {
+	public static Connection getConnection() {
+		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
-			} catch(SQLException e) { 
+			}
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
-		return conn; 
+		return conn;
 	}
 	
-	public static void closeConnection() { 
-		if(conn != null) { 
+	public static void closeConnection() {
+		if (conn != null) {
 			try {
 				conn.close();
-			} catch(SQLException e) { 
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
 	
-
-	private static Properties loadProperties() { 
-		try (FileInputStream fs = new FileInputStream(("db.properties"))) { 
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		} catch (IOException e) { 
+		}
+		catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
 }
