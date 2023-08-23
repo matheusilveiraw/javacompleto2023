8de583c556752c5658bc1c193cab904a01c5f53a
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-23 15:28:43 -0300

    aula 271 fim pomodoro

--- 21-271-demo-inserir-dados/src/application/Program.java
+++ 21-271-demo-inserir-dados/src/application/Program.java
@@ -1,35 +1,49 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
 	public static void main(String[] args) {
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		try {
+		try { 
 			conn = DB.getConnection();
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES " + "(?, ?, ?, ?, ?)");
+			//os pontos de interrogação são placeholders para os valores que virão no setstring abaixo
+			//por exemplo, o primeiro ?, é relativo ao valor um, o segundo ? é relativo ao 2
 			
-			st = conn.createStatement();
+			st.setString(1, "Matheus Carnage");
+			st.setString(2, "matheus@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("01/07/1996").getTime()));
+			st.setDouble(4, 6000.00);
+			st.setInt(5, 4);
 			
-			rs = st.executeQuery("select * from department");
+			int rowsAffected = st.executeUpdate();
 			
-		
-			while(rs.next()) { 
-				System.out.println(rs.getInt("Id") + "-" + rs.getString("Name"));
+			if(rowsAffected > 0) { 
+				st.getGeneratedKeys();
+			} else { 
+				System.out.println("No rows affected!");
 			}
+			
+			
+			System.out.println("Done, rows affected: " + rowsAffected);
 		} catch (SQLException e) { 
 			e.printStackTrace();
-		} finally {
+		} catch (ParseException e) { 
+			e.printStackTrace();
+		} finally { 
 			DB.closeStatement(st);
-			DB.closeResultSet(rs);
 			DB.closeConnection();
 		}
+		
 	}
 }
