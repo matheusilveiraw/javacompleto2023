97bd0f13afdb36523e347970caad320616174c29
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-23 15:43:15 -0300

    aula 271 finalizada

--- 21-271-demo-inserir-dados/src/application/Program.java
+++ 21-271-demo-inserir-dados/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -16,7 +18,7 @@ public class Program {
 		
 		try { 
 			conn = DB.getConnection();
-			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES " + "(?, ?, ?, ?, ?)");
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 			//os pontos de interrogação são placeholders para os valores que virão no setstring abaixo
 			//por exemplo, o primeiro ?, é relativo ao valor um, o segundo ? é relativo ao 2
 			
@@ -26,10 +28,17 @@ public class Program {
 			st.setDouble(4, 6000.00);
 			st.setInt(5, 4);
 			
+//			st = conn.prepareStatement("insert into department (Name) values ('D1'), ('D2')", Statement.RETURN_GENERATED_KEYS);
+			//isso aqui é pra botar dois departmanetos lá na tabela mas aí precisa comentar todo o código pra funfar, vai retornar dois ids
+			
 			int rowsAffected = st.executeUpdate();
 			
 			if(rowsAffected > 0) { 
-				st.getGeneratedKeys();
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()){ 
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
 			} else { 
 				System.out.println("No rows affected!");
 			}
