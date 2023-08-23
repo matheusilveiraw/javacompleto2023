86d655cb3e910e8c6e817ce8f8811581e49139aa
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-23 14:54:04 -0300

    aula 270 pomodor fim

--- 21-270-aulas-basicas-banco-de-dados/src/application/Program.java
+++ 21-270-aulas-basicas-banco-de-dados/src/application/Program.java
@@ -1,12 +1,27 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 	public static void main(String[] args) {
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			st.executeQuery("select * from department");
+			
+			
+		} catch () { 
+			
+		}
 	}
 }
