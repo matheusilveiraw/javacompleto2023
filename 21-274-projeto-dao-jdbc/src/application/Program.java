4064619c88d710f7f8c34cbe7b0170efae759822
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-29 19:11:58 -0300

    att

--- 21-274-projeto-dao-jdbc/src/application/Program.java
+++ 21-274-projeto-dao-jdbc/src/application/Program.java
@@ -23,11 +23,11 @@ public class Program {
 		for(Seller obj : list) { 
 			System.out.println(obj);
 		}
-		
+		 
 		System.out.println("==== TEST 3: seller findBAll: =====");
-		list = sellerDao.findAll();
-		for(Seller obj : list) { 
-			System.out.println(obj);
+		list = sellerDao.findAll(); 
+		for(Seller obj : list) {  
+			System.out.println(obj); 
 		}
 	}
 }
